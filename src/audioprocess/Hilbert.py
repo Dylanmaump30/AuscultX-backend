ebb28b881123caab19b55d0b1a6a40e70f64f93d
Dylanmaump30 <dylanmauricio05@gmail.com>
2025-04-07 10:34:35 -0500

    New changes

--- src/audioprocess/Hilbert.py
+++ src/audioprocess/Hilbert.py
@@ -2,13 +2,11 @@ import numpy as np
 from scipy.signal import hilbert
 
 
-def envelope(signal, fs=16000):
+def envelope(signal, fs=44100):
     analytic_signal = hilbert(signal)
     amplitude_envelope = np.abs(analytic_signal)
     amp_min, amp_max = np.min(amplitude_envelope), np.max(amplitude_envelope)
     amp_norm = (amplitude_envelope - amp_min) / (amp_max - amp_min)
 
-    inst_phase = np.unwrap(np.angle(analytic_signal))
-    inst_freq = np.diff(inst_phase) / (2 * np.pi) * fs
 
     return amp_norm
