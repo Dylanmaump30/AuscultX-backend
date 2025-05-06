2bab5ea0e8952853522250947d6bd422deb01cbc
Dylanmaump30 <dylanmauricio05@gmail.com>
2025-05-05 20:23:04 -0500

    some changes

--- src/audioprocess/Mainprocess.py
+++ src/audioprocess/Mainprocess.py
@@ -3,62 +3,55 @@ import numpy as np
 from src.audioprocess import Butterworth as butter
 from src.audioprocess import Hilbert as hilbert
 from scipy.signal import find_peaks
+from scipy.ndimage import uniform_filter1d
 from matplotlib import pyplot as plt
 from scipy.io import wavfile 
+import math
 
 CUT_FREQ = [250, 2250] 
+def normalize_audio(amp):
+    if amp.dtype == np.int16:
+        return amp.astype(np.float32) / 32768
+    elif amp.dtype == np.int32:
+        return amp.astype(np.float32) / 2147483648
+    elif amp.dtype == np.uint8:
+        return (amp.astype(np.float32) - 128) / 128
+    return amp.astype(np.float32)
+
+def clip_signal(signal):
+    mean = np.mean(signal)
+    std = np.std(signal)
+    return np.clip(signal, mean - 3 * std, mean + 3 * std)
+
+def estimate_peaks(signal, fs):
+    duration = len(signal) / fs
+    est_inhales = duration / 1.0
+    min_distance = int(fs * (duration / est_inhales) * 0.5)
+    threshold = np.percentile(signal, 35)
+    prominence = np.percentile(signal, 25) * 0.9
+    peaks, properties = find_peaks(
+        signal, height=threshold, distance=min_distance, prominence=prominence, 
+    )
+    rpm = math.ceil(((len(peaks) / 2) / duration) * 60)
+
+    return peaks, threshold, len(peaks), rpm, duration
 
 def process_audio(audio_path):
     if not os.path.exists(audio_path):
         print(f"Error: Audio file '{audio_path}' not found.")
         return None
 
+    fs, amp = wavfile.read(audio_path)
+    amp = normalize_audio(amp)
 
-    fs, amp = wavfile.read(audio_path)  
-  
-    
-    original_signal = amp.copy()
-
-    filtered_signal = butter.filter_audio(amp=amp, fs=fs, cut_freq=CUT_FREQ, ftype='band')
-
-    envelope_signal = hilbert.envelope(filtered_signal,fs=fs)
-
+    original_signal = clip_signal(amp.copy())
+    filtered_signal = clip_signal(butter.filter_audio(amp=amp, fs=fs, cut_freq=CUT_FREQ, ftype='band'))
+    envelope_signal = hilbert.envelope(filtered_signal, fs=fs)
     final_signal = butter.filter_audio(amp=envelope_signal, fs=fs, cut_freq=CUT_FREQ[0], ftype='low')
-
-    time = np.linspace(0, len(final_signal) / fs, num=len(final_signal))  
-
-    adaptive_threshold = np.percentile(final_signal, 90)  
-    min_peak_distance = fs * 2  
-
-    peaks, _ = find_peaks(final_signal, height=adaptive_threshold, distance=min_peak_distance)  
-    n_peaks = len(peaks)
-    duracion_seg = time[-1] - time[0]
-    rpm = (n_peaks / duracion_seg) * 60
-  
-    """plt.figure(figsize=(12, 8))
-    plt.subplot(3, 1, 1)
-    plt.plot(time, original_signal, color='#148db6', label="Señal Original")
-    plt.title("Señal Original")
-    plt.xlabel("Tiempo (s)")
-    plt.ylabel("Amplitud")
-
-    plt.subplot(3, 1, 2)
-    plt.plot(time, filtered_signal, color='#148db6', label="Señal Filtrada")
-    plt.title("Señal Filtrada")
-    plt.xlabel("Tiempo (s)")
-    plt.ylabel("Amplitud")
-
-    plt.subplot(3, 1, 3)
-    plt.plot(time, final_signal, color='#148db6', label="Señal Envolvente")
-    plt.scatter(time[peaks], final_signal[peaks], color='black', marker='o', label="Picos detectados")
-    plt.title("Señal Envolvente con Picos Detectados")
-    plt.xlabel("Tiempo (s)")
-    plt.ylabel("Amplitud")
-    plt.legend()
-
-    plt.tight_layout()
-    plt.show() 
-    """
+    final_signal = uniform_filter1d(final_signal, size=int(fs * 0.15) )
+    time = np.linspace(0, len(final_signal) / fs, num=len(final_signal))
+    peaks, threshold, n_peaks, rpm, duration = estimate_peaks(final_signal, fs)
+    
     return {
         "audio_name": os.path.basename(audio_path),
         "frame_rate": fs,
